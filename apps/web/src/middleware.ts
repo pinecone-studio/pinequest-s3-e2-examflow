da5882712a14de9d287cfc39894d85a6ee0f9fb4
TOM-BOSS-pinetour <b94889340@gmail.com>
2026-03-25 19:35:03 +0800

    fixed middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,18 +2,29 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-}, {
-  signInUrl: "/sign-in",
-  signUpUrl: "/sign-up",
-});
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (!isPublicRoute(req)) {
+      await auth.protect();
+    }
+  },
+  (req) => {
+    const isPreviewHost = req.nextUrl.hostname.endsWith(".pages.dev");
+
+    return {
+      signInUrl: "/sign-in",
+      signUpUrl: "/sign-up",
+      debug: isPreviewHost,
+      frontendApiProxy: {
+        enabled: isPreviewHost,
+      },
+    };
+  },
+);
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/(api|trpc|__clerk)(.*)",
   ],
 };
