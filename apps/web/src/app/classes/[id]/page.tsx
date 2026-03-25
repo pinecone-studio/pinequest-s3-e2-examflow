ecf51ca58adbdfd448d1631ca8485b2f42e30514
munkhbayar0017-wq <munkhbayar0017@gmail.com>
2026-03-25 22:00:39 +0800

    fix bug

--- apps/web/src/app/classes/[id]/page.tsx
+++ apps/web/src/app/classes/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { AppShell } from "@/app/components/app-shell";
 import { ClassDetailPageContent } from "./class-detail-page-content";
 
+export const runtime = "edge";
+
 type ClassDetailPageProps = {
   params: Promise<{ id: string }>;
 };
